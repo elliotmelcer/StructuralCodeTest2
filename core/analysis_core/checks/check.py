d6077076f43f806be0945b63ece917da7f270ff8
elliotmmm <3lliotmmm@gmail.com>
2026-01-14 11:41:42 +0100

    Changed the Input for calculateUtilization to - slab_construction: SlabConstruction, - loads: Loads, - system: str, - moment: str

--- core/analysis_core/checks/check.py
+++ core/analysis_core/checks/check.py
@@ -4,14 +4,14 @@ from core.analysis_core.internal_forces import InternalForces
 from core.analysis_core.loads import Loads
 from core.analysis_core.section_methods import calculate_bending_strength_uls
 from core.unit_core import Nmm_to_kNm
-
+from slab_construction.slab_construction import SlabConstruction
 
 
 class StructuralCheck(ABC):
 
     @staticmethod
     @abstractmethod
-    def calculateUtilization(loads: Loads) -> float:
+    def calculateUtilization(slab_construction: SlabConstruction, loads: Loads, system: str, moment: str) -> float:
         """Returns the utilization ratio"""
         raise NotImplementedError
 
@@ -19,7 +19,7 @@ class StructuralCheck(ABC):
 class UltimateMomentCheckEC2004DE(StructuralCheck):
 
     @staticmethod
-    def calculateUtilization(loads: Loads) -> float:
+    def calculateUtilization(slab_construction: SlabConstruction, loads: Loads, system: str = "SIMPLE_BEAM", moment: str = "MAX_POS_MOMENT") -> float:
         slab = loads.slab_construction.slab
 
         MRd = - Nmm_to_kNm(
